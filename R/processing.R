d4bd9d55f488a9186b3ee79cdb35a8c2c084ec93
Taren Sanders <taren.sanders@acu.edu.au>
2021-10-07 12:26:07 +1100

    Add wrapper function

--- R/processing.R
+++ R/processing.R
@@ -19,6 +19,8 @@
 #' file paths to process (as per [GGIR::g.shell.GGIR()]).
 #' @param outputdir the directory to store the output (as per
 #' [GGIR::g.shell.GGIR()]).
+#' @param studyname The name of the study. Only used if `datadir` is a vector of
+#' files.
 #' @param ages The age of the sample. Can be either `"children"` (for those 18
 #' or less), `"adults"` (for >18) or both (i.e., `c("children", "adults")`). If
 #' you do not specify, both are used.
@@ -40,6 +42,7 @@
 #' @export
 process_files <- function(datadir,
                           outputdir,
+                          studyname = "NA",
                           ages = c("children", "adults"),
                           device = c("geneactiv", "actigraph"),
                           wear_location = c("wrist", "hip"),
@@ -77,9 +80,11 @@ process_files <- function(datadir,
     do.enmo = TRUE,
     do.hfen = TRUE,
     do.enmoa = TRUE,
+    studyname = studyname,
     # =====================
     # Part 2
     # =====================
+    do.imp = TRUE,
     strategy = 2,
     maxdur = 0,
     includedaycrit = 16,
@@ -90,6 +95,7 @@ process_files <- function(datadir,
     includenightcrit = 16,
     MX.ig.min.dur = 10,
     iglevels = 50,
+    ilevels = seq(0,4000,50),
     epochvalues2csv = FALSE,
     winhr = 16,
     qlevels = c(
@@ -126,12 +132,121 @@ process_files <- function(datadir,
   )
 }
 
-reprocess <- function(){
-  # TODO - The wrapper function
-  # STEPS
+#' Reprocess Accelerometer Data and Generate Metadata
+#'
+#' This is the primary wrapper function for sleepIPD. In most cases, this should
+#' be the only function that is needed. `reprocess` collects data about your
+#' study, and then calls the functions to reprocess the data, collate the
+#' results, and generate the metadata.
+#'
+#' The purpose of sleepIPD is to standardise the processing of the
+#' accelerometery files. As such, most of the arguments come preset and should
+#' not be modified. However, options which do not affect the output (e.g., the
+#' decision to process in parallel or not) are available via `...`. See
+#' [GGIR::g.shell.GGIR()] for details of these arguments.
+#'
+#' You can specify parameters of your study by supplying `ages`, `device`,
+#' and/or `wear_location` (see Arguments for details). Not supplying these will
+#' cause sleepIPD to calculate every combination of thresholds (resulting in
+#' \eqn{8^3 \times 2 = 1024} output files). So, supplying them should slightly
+#' reduce processing time.
+#'
+#' You should also supply a `studyname` argument. This is required by GGIR if
+#' your `datadir` is a vector of files, but is additionally used by sleepIPD to
+#' name the output files. If your study did not have a name, the last name of
+#' the first author is sufficent (if you are supplying multiple studies, name
+#' each one with the year the data were collected; e.g.,
+#' \code{studyname = 'Hartwig 2021'}).
+#'
+#' @param datadir The directory with the files to process or a vector of
+#' file paths to process (as per [GGIR::g.shell.GGIR()]).
+#' @param outputdir the directory to store the output (as per
+#' [GGIR::g.shell.GGIR()]).
+#' @param studyname The name of the study. This is required to name the
+#' metadata.
+#' @param ages The age of the sample. Can be either `"children"` (for those 18
+#' or less), `"adults"` (for >18) or both (i.e., `c("children", "adults")`). If
+#' you do not specify, both are used.
+#' @param device The device used in the study. Can be either `"geneactiv"`,
+#'  `"actigraph"` or (in the unlikely event two devices were used) both (i.e.,
+#'  `c("geneactiv", "actigraph")`). If you do not specify, both are used.
+#' @param wear_location The location the device was warn. Can be either
+#' `"wrist"`, `"hip"` or (in the unlikely event multiple locations were used)
+#' both (i.e., `c("wrist","hip")`). If you do not specify, both are used.
+#' @param overwrite If the existing output (including metadata) should be
+#' replaced. `FALSE` by default.
+#' @param verbose Determines how much output the function returns. Useful for
+#' understanding what the package is doing. `TRUE` by default.
+#' @param ... Pass additional parameters to [GGIR::g.shell.GGIR()]. Refer to the
+#' `GGIR` manual for details.
+#'
+#' @family processing functions
+#' @export
+#'
+#' @examples
+reprocess <- function(datadir,
+                      outputdir,
+                      studyname,
+                      ages = c("children", "adults"),
+                      device = c("geneactiv", "actigraph"),
+                      wear_location = c("wrist", "hip"),
+                      overwrite = FALSE,
+                      verbose = TRUE,
+                      ...) {
+  start_time <- Sys.time()
+  if (verbose) usethis::ui_info("Starting {usethis::ui_code('process_files()')}")
+  process_files(
+    datadir, outputdir, studyname, ages, device,
+    wear_location, overwrite, verbose
+  )
+  end_process_files <- Sys.time()
 
-  # 1. Run process_files (includes validation check)
-  # 2. Collate the returned files into a single csv
-  # 3. Generate the meta_data
+  if (verbose) {
+    print("/n")
+    usethis::ui_done("Completed {usethis::ui_code('process_files()')}")
+    usethis::ui_info("Starting {usethis::ui_code('collate_outputs()')}")
+  }
 
+  collate_outputs(outputdir, studyname, verbose)
+  end_collate_outputs <- Sys.time()
+
+  if (verbose) {
+    usethis::ui_done("Completed {usethis::ui_code('collate_outputs()')}")
+    usethis::ui_info("Starting {usethis::ui_code('build_meta()')}")
+  }
+
+  build_meta(outputdir, studyname, overwrite, wear_location)
+  end_build_meta <- Sys.time()
+
+  if (verbose) usethis::ui_done("Completed {usethis::ui_code('build_meta()')}")
+
+  if (verbose & requireNamespace("knitr", quietly = TRUE)) {
+    usethis::ui_done("Processing complete! Here is how long it took:")
+
+    opts <- options(knitr.kable.NA = "")
+
+    print(
+    knitr::kable(data.frame(
+      "Process" = c(
+        "GGIR Processing", "Collate Outputs",
+        "Build Metadata", "Total"
+      ),
+      "Start Time" = c(
+        start_time, end_process_files,
+        end_collate_outputs, NA
+      ),
+      "End Time" = c(
+        end_process_files, end_collate_outputs,
+        end_build_meta, NA
+      ),
+      "Duration" = c(
+        difftime(end_process_files, start_time),
+        difftime(end_collate_outputs, end_process_files),
+        difftime(end_build_meta, end_collate_outputs),
+        difftime(end_build_meta, start_time)
+      )
+    ), format = "simple"))
+
+    options(opts)
+  }
 }
