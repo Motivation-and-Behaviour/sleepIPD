2597a69ec516a546d3f6a163d983522609564bb5
Taren Sanders <taren.sanders@acu.edu.au>
2021-10-07 16:57:25 +1100

    Updated thresholds

--- data-raw/thresholds.R
+++ data-raw/thresholds.R
@@ -1,14 +1,15 @@
 # These are the thresholds for the intensity cut-points. These come from
-# Hildebrand 2014 (https://doi.org/10.1249/mss.0000000000000289) and
-# Hildebrand 2016 (https://doi.org/10.1111/sms.12795)
+# Hildebrand 2014 (https://doi.org/10.1249/mss.0000000000000289),
+# Hildebrand 2016 (https://doi.org/10.1111/sms.12795),
+# Hurter 2018 (https://doi.org/10.3390/children5120172) using non-dominant hand
 
 thresholds <-
   dplyr::tribble(
         ~age,        ~dev, ~wear_loc, ~light,  ~mod,  ~vig,
-  "children", "geneactiv",   "wrist",   56.3, 191.6, 695.8,
-  "children", "actigraph",   "wrist",   35.6, 201.4,   707,
+  "children", "geneactiv",   "wrist",   51.6, 191.6, 695.8,
+  "children", "actigraph",   "wrist",   48.1, 201.4,   707,
   "children", "geneactiv",     "hip",   64.1, 152.8, 514.3,
-  "children", "actigraph",     "hip",   63.3, 142.6, 464.6,
+  "children", "actigraph",     "hip",   32.6, 142.6, 464.6,
   "adults",   "geneactiv",   "wrist",   45.8,  93.2, 418.3,
   "adults",   "actigraph",   "wrist",   44.8, 100.6, 428.8,
   "adults",   "geneactiv",     "hip",   46.9,  68.7, 266.8,
