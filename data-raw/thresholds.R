7948ac6ed32a4effde545eb118b03a4abaada771
Taren Sanders <taren.sanders@acu.edu.au>
2021-10-12 10:36:49 +1100

    Add source column to thresholds

--- data-raw/thresholds.R
+++ data-raw/thresholds.R
@@ -5,15 +5,31 @@
 
 thresholds <-
   dplyr::tribble(
-        ~age,        ~dev, ~wear_loc, ~light,  ~mod,  ~vig,
-  "children", "geneactiv",   "wrist",   51.6, 191.6, 695.8,
-  "children", "actigraph",   "wrist",   48.1, 201.4,   707,
-  "children", "geneactiv",     "hip",   64.1, 152.8, 514.3,
-  "children", "actigraph",     "hip",   32.6, 142.6, 464.6,
-  "adults",   "geneactiv",   "wrist",   45.8,  93.2, 418.3,
-  "adults",   "actigraph",   "wrist",   44.8, 100.6, 428.8,
-  "adults",   "geneactiv",     "hip",   46.9,  68.7, 266.8,
-  "adults",   "actigraph",     "hip",   47.4,  61.1, 258.7
+        ~age,        ~dev, ~wear_loc, ~light,  ~mod,  ~vig, ~source,
+  "children", "geneactiv",   "wrist",   51.6, 191.6, 695.8, "Light: [Hurter et al., 2018](https://doi.org/10.3390/children5120172)</br>
+                                                             Mod: [Hildebrand et al., 2014](https://doi.org/10.1249/mss.0000000000000289)</br>
+                                                             Vig: [Hildebrand et al., 2014](https://doi.org/10.1249/mss.0000000000000289)",
+  "children", "actigraph",   "wrist",   48.1, 201.4,   707, "Light: [Hurter et al., 2018](https://doi.org/10.3390/children5120172)</br>
+                                                             Mod: [Hildebrand et al., 2014](https://doi.org/10.1249/mss.0000000000000289)</br>
+                                                             Vig: [Hildebrand et al., 2014](https://doi.org/10.1249/mss.0000000000000289)",
+  "children", "geneactiv",     "hip",   64.1, 152.8, 514.3, "Light: [Hildebrand et al., 2016](https://doi.org/10.1111/sms.12795)</br>
+                                                             Mod: [Hildebrand et al., 2014](https://doi.org/10.1249/mss.0000000000000289)</br>
+                                                             Vig: [Hildebrand et al., 2014](https://doi.org/10.1249/mss.0000000000000289)",
+  "children", "actigraph",     "hip",   32.6, 142.6, 464.6, "Light: [Hurter et al., 2018](https://doi.org/10.3390/children5120172)</br>
+                                                             Mod: [Hildebrand et al., 2014](https://doi.org/10.1249/mss.0000000000000289)</br>
+                                                             Vig: [Hildebrand et al., 2014](https://doi.org/10.1249/mss.0000000000000289)",
+  "adults",   "geneactiv",   "wrist",   45.8,  93.2, 418.3, "Light: [Hildebrand et al., 2016](https://doi.org/10.1111/sms.12795)</br>
+                                                             Mod: [Hildebrand et al., 2014](https://doi.org/10.1249/mss.0000000000000289)</br>
+                                                             Vig: [Hildebrand et al., 2014](https://doi.org/10.1249/mss.0000000000000289)",
+  "adults",   "actigraph",   "wrist",   44.8, 100.6, 428.8, "Light: [Hildebrand et al., 2016](https://doi.org/10.1111/sms.12795)</br>
+                                                             Mod: [Hildebrand et al., 2014](https://doi.org/10.1249/mss.0000000000000289)</br>
+                                                             Vig: [Hildebrand et al., 2014](https://doi.org/10.1249/mss.0000000000000289)",
+  "adults",   "geneactiv",     "hip",   46.9,  68.7, 266.8, "Light: [Hildebrand et al., 2016](https://doi.org/10.1111/sms.12795)</br>
+                                                             Mod: [Hildebrand et al., 2014](https://doi.org/10.1249/mss.0000000000000289)</br>
+                                                             Vig: [Hildebrand et al., 2014](https://doi.org/10.1249/mss.0000000000000289)",
+  "adults",   "actigraph",     "hip",   47.4,  61.1, 258.7, "Light: [Hildebrand et al., 2016](https://doi.org/10.1111/sms.12795)</br>
+                                                             Mod: [Hildebrand et al., 2014](https://doi.org/10.1249/mss.0000000000000289)</br>
+                                                             Vig: [Hildebrand et al., 2014](https://doi.org/10.1249/mss.0000000000000289)"
 )
 
 
