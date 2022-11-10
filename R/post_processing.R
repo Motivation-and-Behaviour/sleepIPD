0288bdab54c12f8d456ce3d4e5342c08a73d4696
Taren Sanders <Taren.Sanders@acu.edu.au>
2022-11-10 21:06:47 +1100

    WIP: Skelton for `assemble_datasheet`

---
+++ R/post_processing.R
@@ -0,0 +1,42 @@
+assemble_datasheet <- function(folder) {
+  folder_files <- list.files(folder, pattern = ".csv$")
+
+  # Part 2 file
+  part2_filepath <-
+    file.path(folder, folder_files[grepl("^part2", folder_files)])
+
+  part2_df <- readr::read_csv(part2_filepath)
+
+  part2_df <-
+    part2_df %>%
+    select(
+      # Meta
+      filename, `N valid hours`, `N hours`, weekday, measurementday,
+      # Intensity gradient
+      `ig_gradient_ENMO_0-24hr`, `ig_intercept_ENMO_0-24hr`,
+      `ig_rsquared_ENMO_0-24hr`,
+      `M16hr_ENMO_mg_0-24hr`, `M16_ENMO_mg_0-24hr`,
+      `M16_ig_gradient_ENMO_mg_0-24hr`, `M16_ig_intercept_ENMO_mg_0-24hr`,
+      `M16_ig_rsquared_ENMO_mg_0-24hr`, matches("^p.*_ENMO_mg_0-24hr$")
+    )
+
+  # Part 4 file
+  part4_filepath <-
+    file.path(folder, folder_files[grepl("^part4", folder_files)])
+
+  part4_df <- readr::read_csv(part4_filepath)
+
+  # TODO: Select the columns that we want to keep
+
+  # Part 5 file
+  part5_filepath <-
+    file.path(folder, folder_files[grepl("^part5", folder_files)])
+
+  part5_df <- readr::read_csv(part5_filepath)
+
+  # TODO: Select the columns that we want to keep
+
+  # TODO: Merge the dataframes together
+
+  # TODO: Write out the data
+}
