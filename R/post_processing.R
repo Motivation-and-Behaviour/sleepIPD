32f140bd5565dbaf345571ecd88cfaa5b410cd20
Taren Sanders <taren.sanders@acu.edu.au>
2022-11-18 16:09:54 +1100

    WIP: assemble function

--- R/post_processing.R
+++ R/post_processing.R
@@ -37,18 +37,18 @@ assemble_datasheet <- function(folder) {
   part4_filepath <-
     file.path(folder, folder_files[grepl("^part4", folder_files)])
 
-  # TODO: these columns are different if hip-worn is used
   part4_cols <- c(
     # Meta
     "filename", "weekday", "calendar_date", "night",
     # Sleep meta
     "guider", "daysleeper", "sleepparam", "fraction_night_invalid",
-    "nonwear_perc_spt",
     # Sleep data
     "sleeponset", "wakeup", "SptDuration", "guider_onset", "guider_wakeup",
-    "guider_SptDuration", "SleepDurationInSpt", "WASO", "sleeponset_ts",
-    "wakeup_ts", "guider_onset_ts", "guider_wakeup_ts",
-    "SleepRegularityIndex", "SriFractionValid"
+    "guider_SptDuration", "guider_inbedStart", "guider_inbedEnd",
+    "guider_inbedDuration", "guider_inbedStart_ts", "guider_inbedEnd_ts",
+    "SleepDurationInSpt", "WASO", "sleeponset_ts", "wakeup_ts",
+    "guider_onset_ts", "guider_wakeup_ts", "SleepRegularityIndex",
+    "SriFractionValid", "sleeplatency", "sleepefficiency"
   )
 
   part4_df <- readr::read_csv(part4_filepath) %>%
@@ -58,20 +58,22 @@ assemble_datasheet <- function(folder) {
   part5_filepath <-
     file.path(folder, folder_files[grepl("^part5", folder_files)])
 
-  # TODO: this bit
+  # TODO: check if this can be further reduced
   part5_cols <- c(
-    "p5filename", "ID", "filename", "window_number", "weekday",
-    "calendar_date", "sleeponset", "sleeponset_ts", "wakeup", "wakeup_ts",
-    "night_number", "daysleeper", "cleaningcode", "guider", "sleeplog_used",
-    "acc_available", "nonwear_perc_day", "nonwear_perc_spt", "nonwear_perc_day_spt",
-    "dur_spt_sleep_min", "dur_spt_wake_IN_min", "dur_spt_wake_LIG_min",
-    "dur_spt_wake_MOD_min", "dur_spt_wake_VIG_min", "dur_day_IN_unbt_min",
-    "dur_day_LIG_unbt_min", "dur_day_MOD_unbt_min", "dur_day_VIG_unbt_min",
-    "dur_day_MVPA_bts_1_min", "dur_day_IN_bts_30_min", "dur_day_IN_bts_10_30_min",
-    "dur_day_IN_bts_1_10_min", "dur_day_LIG_bts_10_min", "dur_day_LIG_bts_1_10_min",
-    "dur_day_total_IN_min", "dur_day_total_LIG_min", "dur_day_total_MOD_min",
-    "dur_day_total_VIG_min", "dur_day_min", "dur_spt_min", "dur_day_spt_min",
-    "N_atleast5minwakenight", "sleep_efficiency", "ACC_spt_sleep_mg",
+    # Meta
+    "filename", "window_number", "weekday", "calendar_date", "night_number",
+    "nonwear_perc_day", "nonwear_perc_spt",
+    "nonwear_perc_day_spt", "daytype",
+    # PA and Sleep
+    "sleeponset", "sleeponset_ts", "wakeup", "wakeup_ts", "dur_spt_sleep_min",
+    "dur_spt_wake_IN_min", "dur_spt_wake_LIG_min", "dur_spt_wake_MOD_min",
+    "dur_spt_wake_VIG_min", "dur_day_IN_unbt_min", "dur_day_LIG_unbt_min",
+    "dur_day_MOD_unbt_min", "dur_day_VIG_unbt_min", "dur_day_MVPA_bts_1_min",
+    "dur_day_IN_bts_30_min", "dur_day_IN_bts_10_30_min",
+    "dur_day_IN_bts_1_10_min", "dur_day_LIG_bts_10_min",
+    "dur_day_LIG_bts_1_10_min", "dur_day_total_IN_min", "dur_day_total_LIG_min",
+    "dur_day_total_MOD_min", "dur_day_total_VIG_min", "dur_day_min",
+    "dur_spt_min", "dur_day_spt_min", "sleep_efficiency", "ACC_spt_sleep_mg",
     "ACC_spt_wake_IN_mg", "ACC_spt_wake_LIG_mg", "ACC_spt_wake_MOD_mg",
     "ACC_spt_wake_VIG_mg", "ACC_day_IN_unbt_mg", "ACC_day_LIG_unbt_mg",
     "ACC_day_MOD_unbt_mg", "ACC_day_VIG_unbt_mg", "ACC_day_MVPA_bts_1_mg",
@@ -85,21 +87,23 @@ assemble_datasheet <- function(folder) {
     "L0.5VALUE", "M0.5TIME", "M0.5VALUE", "L0.5TIME_num", "M0.5TIME_num",
     "L0.25TIME", "L0.25VALUE", "M0.25TIME", "M0.25VALUE", "L0.25TIME_num",
     "M0.25TIME_num", "L0.166666666666667TIME", "L0.166666666666667VALUE",
-    "M0.166666666666667TIME", "M0.166666666666667VALUE", "L0.166666666666667TIME_num",
-    "M0.166666666666667TIME_num", "L0.0833333333333333TIME", "L0.0833333333333333VALUE",
-    "M0.0833333333333333TIME", "M0.0833333333333333VALUE", "L0.0833333333333333TIME_num",
-    "M0.0833333333333333TIME_num", "Nbouts_day_MVPA_bts_1", "Nbouts_day_IN_bts_30",
-    "Nbouts_day_IN_bts_10_30", "Nbouts_day_IN_bts_1_10", "Nbouts_day_LIG_bts_10",
+    "M0.166666666666667TIME", "M0.166666666666667VALUE",
+    "L0.166666666666667TIME_num", "M0.166666666666667TIME_num",
+    "L0.0833333333333333TIME", "L0.0833333333333333VALUE",
+    "M0.0833333333333333TIME", "M0.0833333333333333VALUE",
+    "L0.0833333333333333TIME_num", "M0.0833333333333333TIME_num",
+    "Nbouts_day_MVPA_bts_1", "Nbouts_day_IN_bts_30", "Nbouts_day_IN_bts_10_30",
+    "Nbouts_day_IN_bts_1_10", "Nbouts_day_LIG_bts_10",
     "Nbouts_day_LIG_bts_1_10", "Nblocks_spt_sleep", "Nblocks_spt_wake_IN",
     "Nblocks_spt_wake_LIG", "Nblocks_spt_wake_MOD", "Nblocks_spt_wake_VIG",
     "Nblocks_day_IN_unbt", "Nblocks_day_LIG_unbt", "Nblocks_day_MOD_unbt",
     "Nblocks_day_VIG_unbt", "Nblocks_day_MVPA_bts_1", "Nblocks_day_IN_bts_30",
-    "Nblocks_day_IN_bts_10_30", "Nblocks_day_IN_bts_1_10", "Nblocks_day_LIG_bts_10",
-    "Nblocks_day_LIG_bts_1_10", "Nblocks_day_total_IN", "Nblocks_day_total_LIG",
-    "Nblocks_day_total_MOD", "Nblocks_day_total_VIG", "boutcriter.in",
-    "boutcriter.lig", "boutcriter.mvpa", "boutdur.in", "boutdur.lig",
-    "boutdur.mvpa", "bout.metric", "ig_gradient", "ig_intercept",
-    "ig_rsquared", "daytype"
+    "Nblocks_day_IN_bts_10_30", "Nblocks_day_IN_bts_1_10",
+    "Nblocks_day_LIG_bts_10", "Nblocks_day_LIG_bts_1_10",
+    "Nblocks_day_total_IN", "Nblocks_day_total_LIG", "Nblocks_day_total_MOD",
+    "Nblocks_day_total_VIG", "boutcriter.in", "boutcriter.lig",
+    "boutcriter.mvpa", "boutdur.in", "boutdur.lig", "boutdur.mvpa",
+    "bout.metric", "ig_gradient", "ig_intercept", "ig_rsquared"
   )
 
   thresh_strings <-
@@ -109,14 +113,12 @@ assemble_datasheet <- function(folder) {
       thresh_age = age, thresh_dev = dev, thresh_wear_loc = wear_loc
     )
 
-  part5_df <- readr::read_csv(part5_filepath)
-
-  part5_df %>%
+  part5_df <- readr::read_csv(part5_filepath) %>%
     dplyr::filter(
       # Just use the MM files
       stringr::str_detect(p5filename, "^part5_daysummary_MM") &
         stringr::str_detect(
-          p5filename, paste(pull(thresh_strings, thresh), collapse = "|")
+          p5filename, paste(dplyr::pull(thresh_strings, thresh), collapse = "|")
         )
     ) %>%
     fuzzyjoin::regex_left_join(
@@ -128,8 +130,40 @@ assemble_datasheet <- function(folder) {
       tidyselect::starts_with("thresh_")
     )
 
+  # Make the dataframes consistent
+  part2_df_clean <-
+    part2_df %>%
+    dplyr::mutate(calendar_date = lubridate::as_date(calendar_date))
+
+  part4_df_clean <-
+    part4_df %>%
+    dplyr::mutate(
+      calendar_date = lubridate::as_date(calendar_date, format = "%d/%m/%Y"),
+      filename = stringr::str_replace(filename, ".RData", "")
+    ) %>%
+    dplyr::rename(
+      sleeponset_p4 = sleeponset, wakeup_p4 = wakeup,
+      sleeponset_ts_p4 = sleeponset_ts, wakeup_ts_p4 = wakeup_ts
+    )
+
+  part5_df_clean <-
+    part5_df %>%
+    dplyr::mutate(
+      calendar_date = lubridate::as_date(calendar_date, format = "%d/%m/%Y"),
+      filename = stringr::str_replace(filename, ".RData", "")
+    ) %>%
+    dplyr::rename(
+      sleeponset_p5 = sleeponset, wakeup_p5 = wakeup,
+      sleeponset_ts_p5 = sleeponset_ts, wakeup_ts_p5 = wakeup_ts
+    )
 
   # TODO: Merge the dataframes together
 
   # TODO: Write out the data
 }
+
+part2_df %>%
+  select(measurementday) %>%
+  head()
+
+intersect(p5_cols, p2_cols)
